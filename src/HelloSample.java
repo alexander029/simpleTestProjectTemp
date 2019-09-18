9d2f1305c02b1da35d37d7864c566554ff587968
Aleksandr Ustianskii <9383780@gmail.com>
2019-09-18 17:48:53 +0300

    commit1

--- src/HelloSample.java
+++ src/HelloSample.java
@@ -1,5 +1,8 @@
-public class HelloSample {
+package src;
+
+public class HelloSample  {
     public static void main(String[] args){
         System.out.println("Hello Word");
     }
 }
+/*git commit1
